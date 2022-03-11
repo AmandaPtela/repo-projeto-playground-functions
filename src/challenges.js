1aa46d74e2b2bad1ddfcc6607570ca0d1f83c333
Amanda Portela <mandaptela@gmail.com>
2022-03-11 15:40:16 -0300

    ok2

--- src/challenges.js
+++ src/challenges.js
@@ -1,22 +1,22 @@
-//Amanda Karoline
+// Amanda Karoline
 // Desafio 1
 function compareTrue(a, b) {
   if (a > b && b < a === true) {
-    return("true")
+    return ("true")
   }
   else if (a > b && a < b) {
-    return("false")
+    return ('false')
   }
   else {
-    return("false")
+    return ('false')
   }
 }
 compareTrue(50, 8)
 
 // Desafio 2 - feito
 function calcArea(base, height) {
-  const area = base * height / 2;
-  return(area);
+  const area = (base * height) / 2;
+  return (area);
 }
 calcArea(51, 1);
 // Desafio 3
