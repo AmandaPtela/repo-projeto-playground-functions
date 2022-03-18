c9164aeb7ba1fd8eb3f365ada2b86f6660b58a4c
Amanda Portela <mandaptela@gmail.com>
2022-03-18 16:39:02 -0500

    q. 8

--- src/challenges.js
+++ src/challenges.js
@@ -2,15 +2,12 @@
 // Desafio 1
 function compareTrue(a, b) {
   if (a === true && b === true) {
-    return (true);
-  }
-
-  else if (a === true && b === false) {
-    return (false)
-  }
-
-  else (a === false && b === false); {
-    return (false)
+    return true;
+  } else if (a === true && b === false) {
+    return false;
+  } else a === false && b === false;
+  {
+    return false;
   }
 }
 compareTrue(true, true);
@@ -18,68 +15,89 @@ compareTrue(true, true);
 // Desafio 2 - feito - ok
 function calcArea(base, height) {
   const area = (base * height) / 2;
-  return (area);
+  return area;
 }
 calcArea(51, 1);
 
 // Desafio 3
 
 function splitSentence(frase) {
-  for (i = 0 ; i <= frase.length; i += 1){
-    let splited = frase.split(" ")
-  return(splited)}
+  for (i = 0; i <= frase.length; i += 1) {
+    let splited = frase.split(' ');
+    return splited;
+  }
 }
-splitSentence("Vamo que vamo")
+splitSentence('Vamo que vamo');
 
 // Desafio 4
 function concatName(palavras, palavras2, palavras3) {
-  let concat ;
-  for (i = 0; i <= palavras.length; i += 1){
-    concat = palavras.slice(-1) + ", " + palavras[0];
-    return(concat)}
-    for (i = 0; i <= palavras2.length; i += 1){
-      concat = palavras2.slice(-1) + ", " + palavras2[0];
-      return(concat)}
-      for (i = 0; i <= palavras3.length; i += 1){
-        concat = palavras3.slice(-1) + ", " + palavras3[0];
-        return(concat)}
+  let concat;
+  for (i = 0; i <= palavras.length; i += 1) {
+    concat = palavras.slice(-1) + ', ' + palavras[0];
+    return concat;
+  }
+  for (i = 0; i <= palavras2.length; i += 1) {
+    concat = palavras2.slice(-1) + ', ' + palavras2[0];
+    return concat;
+  }
+  for (i = 0; i <= palavras3.length; i += 1) {
+    concat = palavras3.slice(-1) + ', ' + palavras3[0];
+    return concat;
+  }
 }
-concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], ['foguete', 'não', 'tem', 'ré'], ['captain', 'my', 'captain'])
+concatName(
+  ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'],
+  ['foguete', 'não', 'tem', 'ré'],
+  ['captain', 'my', 'captain']
+);
 
 // Desafio 5
 let pontosWins = 3;
 let pontosTies = 1;
 
 function footballPoints(wins, ties) {
-  let pontosTotal = (wins * pontosWins) + (ties * pontosTies);
-  return (pontosTotal)}
+  let pontosTotal = wins * pontosWins + ties * pontosTies;
+  return pontosTotal;
+}
 
 footballPoints(14, 8);
 
 // Desafio 6
-function highestCount(numeros){
+function highestCount(numeros) {
   let maior = Math.max(...numeros);
   let vezes = 0;
 
-  for (i = 0; i <= numeros.length; i +=1){
-    if (numeros[i] == maior){
-      vezes +=1;
+  for (i = 0; i <= numeros.length; i += 1) {
+    if (numeros[i] == maior) {
+      vezes += 1;
     }
   }
-  return(vezes);
+  return vezes;
 }
-highestCount([9, 1, 2, 3, 9, 5, 7])
+highestCount([9, 1, 2, 3, 9, 5, 7]);
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1, cat2) {}
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+function fizzBuzz(numeross) {
+  let resultado = [];
 
+  for (i = 0; i < numeross.length; i += 1) {
+    if (numeross[i] % 3 == 0 && numeross[i] % 5 == 0) {
+      resultado.push('fizzBuzz');
+    } else if (numeross[i] % 5 == 0) {
+      resultado.push('buzz');
+    } else if (numeross[i] %3 == 0){
+      resultado.push("fizz");
+    }
+     else {
+      resultado.push('bug!');
+    }
+  }
+  return(resultado);
+}
+fizzBuzz([2, 15, 7, 9, 45]);
 // Desafio 9
 function encode(decifrar) {}
 
