bb4e2f140f19162ff9b91f0a04d39f8251838b30
Amanda Portela <mandaptela@gmail.com>
2022-03-22 09:14:04 -0500

    q.11 talvez

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,36 +1,52 @@
 // Desafio 11
 function generatePhoneNumber(numeroTel) {
+  for (i = 0; i <= numeroTel.length; i += 1) {
+    let telefone = numeroTel.slice(2, 7);
+    let telefone2 = numeroTel.slice(7,11);
+    let ddd = numeroTel.slice(0, 2);
+    let numero = '(' + ddd.join('') + ')' + ' ' + telefone.join('') + '-' + telefone2.join('');
 
+    if (numeroTel.length !== 11) {
+      return ('Array com tamanho incorreto.');
+    } 
+    
+    else if (numero[i] < '0' || numero[i] > '9' || numero[0] === '0' ) {
+      return('não é possível gerar um número de telefone com esses valores');
+    }
+    else if (numero[i] > '0' && numero[i] < '9' && numero[0] !== '0');{
+      return (numero);
+    }
+  }
 }
-generatePhoneNumber(1234567890)
+generatePhoneNumber(['1','2','3','4','5','6','7','8','9','0','1']);
 
 // Desafio 12
+
+
 function triangleCheck(lineA, lineB, lineC) {
-  let absLineBeC = Math.abs(lineB-lineC);
-  let absLineAeC = Math.abs(lineA-lineC);
-  let absLineBeA = Math.abs(lineB-lineA)
+  let absLineBeC = Math.abs(lineB - lineC);
+  let absLineAeC = Math.abs(lineA - lineC);
+  let absLineBeA = Math.abs(lineB - lineA);
 
-  let vero = lineA < (lineB+lineC) || lineB < (lineA+lineC) || lineC < (lineB+lineA);
-  let veroAbs = lineA || lineB ||lineC > absLineAeC || absLineBeA || absLineBeC;
+  let vero =
+    lineA < lineB + lineC || lineB < lineA + lineC || lineC < lineB + lineA;
+  let veroAbs =
+    lineA || lineB || lineC > absLineAeC || absLineBeA || absLineBeC;
 
-  let mentira = lineA > (lineB+lineC) || lineB > (lineA+lineC) || lineC > (lineB+lineA);
+  let mentira =
+    lineA > lineB + lineC || lineB > lineA + lineC || lineC > lineB + lineA;
   let mentiraAbs = mentira < absLineAeC && absLineBeA && absLineBeC;
   let triangulo = true;
 
-   if (mentira && mentiraAbs){
-    return(!triangulo)
+  if (mentira && mentiraAbs) {
+    return !triangulo;
+  } else if (mentira) {
+    return !triangulo;
+  } else if (vero && veroAbs) {
+    return triangulo;
+  } else {
+    return triangulo;
   }
-  else if(mentira){
-    return(!triangulo)
-  }
-
-  else if (vero && veroAbs){
-    return(triangulo);
-  }
-
-   else {
-     return(triangulo);
-   }
 }
 triangleCheck(10, 14, 8);
 
